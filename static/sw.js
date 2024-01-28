842bc47599175204931d6083f7e76fe7bc440dfa
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2024-01-28 11:14:19 +0530

    --

--- static/sw.js
+++ static/sw.js
@@ -14,6 +14,8 @@ const BASE_CACHE_FILES = [
     'https://www.googletagmanager.com/gtag/js?id=G-CP4QE6ZEV0',
     'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.min.js',
     'https://samirpaul.disqus.com/embed.js',
+    'https://samirpaul.disqus.com/count.js',
+
 ];
 
 const OFFLINE_CACHE_FILES = [
